86cb7c1cf52495f0dc77e97109c3373177c77383
Liam Nickell <liam.nickell25@gmail.com>
2016-08-28 01:36:40 -0500

    reorganized code and simplified var names

--- lorenz_attractor.py
+++ lorenz_attractor.py
@@ -3,19 +3,14 @@
 #
 # This program was made in order to help me learn more about the Lorenz Equations, Python, and matplotlib
 # Go read "Chaos: Making a New Science" if you want to read about Chaos Theory (including Lorenz)
-#
-# Note: Some variable are intentionally long/overly descriptive (e.g. "x" vs "x_point") for readability 
-# and explainability to those who are not programmers (as well as for myself so that each variable or 
-# parameter is easy to keep track of)
 
 __author__ = "github.com/liamnickell"
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-num_of_points = 9250
 
-def get_points(x, y, z, sigma=10.0, rho=28.0, beta=8.0/3.0, time=0.01):
+def get_values(x, y, z, sigma=10.0, rho=28.0, beta=8.0/3.0, time=0.01):
 	x_point = time * (sigma * (y - x))
 	y_point = time * (x * (rho - z) - y)
 	z_point = time * ((x * y) - (beta * z))
@@ -23,23 +18,30 @@ def get_points(x, y, z, sigma=10.0, rho=28.0, beta=8.0/3.0, time=0.01):
 	return x_point, y_point, z_point
 
 
-def main():
-	x_points, y_points, z_points = [0.0], [1.0], [1.05]
+def generate_points():
+	num_of_points = 9000
+	xs, ys, zs = [0.0], [1.0], [1.05]
 
 	for i in range(num_of_points):
-		new_x, new_y, new_z = get_points(x_points[i], y_points[i], z_points[i])
-		x_points.append(x_points[i] + new_x)
-		y_points.append(y_points[i] + new_y)
-		z_points.append(z_points[i] + new_z)
+		new_x, new_y, new_z = get_values(xs[i], ys[i], zs[i])
+		xs.append(xs[i] + new_x)
+		ys.append(ys[i] + new_y)
+		zs.append(zs[i] + new_z)
+
+	return xs, ys, zs
+
+
+def main():
+	xs, ys, zs = generate_points()
 
 	fig = plt.figure()
 	ax = fig.gca(projection='3d')
 
-	ax.plot(x_points, y_points, z_points)
+	ax.plot(xs, ys, zs)
+	ax.set_title("Lorenz Attractor")
 	ax.set_xlabel("X Axis")
 	ax.set_ylabel("Y Axis")
 	ax.set_zlabel("Z Axis")
-	ax.set_title("Lorenz Attractor")
 
 	plt.show()
 
