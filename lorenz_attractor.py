113a102a5caff10043bd18d37edfe5d4f628b98e
Liam Nickell <liam.nickell25@gmail.com>
2017-03-04 09:42:51 -0600

    changed tabs to spaces

--- lorenz_attractor.py
+++ lorenz_attractor.py
@@ -4,51 +4,51 @@
 # This program was made in order to help me learn more about the Lorenz Equations, Python, and matplotlib
 # Go read "Chaos: Making a New Science" if you want to read about Chaos Theory (including Lorenz)
 
-__author__ = "github.com/liamnickell"
+__author__ = 'github.com/liamnickell'
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-# Change the values of sigma, rho, and/or beta (and/or time) in the default param value or function
+# Change the values of sigma, rho, beta, and/or time in the default param value or function
 # call to manipulate the appearance of the Lorenz Attractor
-def get_values(x, y, z, sigma=10.0, rho=28.0, beta=8.0/3.0, time=0.01):
-	x_point = time * (sigma * (y - x))
-	y_point = time * (x * (rho - z) - y)
-	z_point = time * ((x * y) - (beta * z))
+def get_values(x, y, z, sigma=10.000001, rho=28.0, beta=8.0/3.0, time=0.01):
+    x_point = time * (sigma * (y - x))
+    y_point = time * (x * (rho - z) - y)
+    z_point = time * ((x * y) - (beta * z))
 
-	return x_point, y_point, z_point
+    return x_point, y_point, z_point
 
 
-# The higher the value of points, the more points are graphed (and vice versa)
+# Points is number of points graphed
 def generate_points(points=9000):
-	xs, ys, zs = [0.0], [1.0], [1.05]
+    xs, ys, zs = [0.0], [1.0], [1.05]
 
-	for i in range(points):
-		new_x, new_y, new_z = get_values(xs[i], ys[i], zs[i])
-		xs.append(xs[i] + new_x)
-		ys.append(ys[i] + new_y)
-		zs.append(zs[i] + new_z)
+    for i in range(points):
+        new_x, new_y, new_z = get_values(xs[i], ys[i], zs[i])
+        xs.append(xs[i] + new_x)
+        ys.append(ys[i] + new_y)
+        zs.append(zs[i] + new_z)
 
-	return xs, ys, zs
+    return xs, ys, zs
 
 
 def main():
-	xs, ys, zs = generate_points()
+    xs, ys, zs = generate_points()
 
-	fig = plt.figure()
-	ax = fig.add_subplot(111, projection='3d')
-	ax.grid(False) # Change to True to show grid lines
-	plt.axis('on') # Change to 'off' to turn off axes
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.grid(False)
+    plt.axis('on')
 
-	ax.plot(xs, ys, zs)
-	ax.set_title("Lorenz Attractor")
-	ax.set_xlabel("X Axis")
-	ax.set_ylabel("Y Axis")
-	ax.set_zlabel("Z Axis")
+    ax.plot(xs, ys, zs)
+    ax.set_title("Lorenz Attractor")
+    ax.set_xlabel("X Axis")
+    ax.set_ylabel("Y Axis")
+    ax.set_zlabel("Z Axis")
 
-	plt.show()
+    plt.show()
 
 
 if __name__ == "__main__":
-	main()
+    main()
